7024f6c422877b06e7ff737d7db73c5de16fb695
Lucas Silva <lucassilva080397@gmail.com>
2023-11-17 17:53:38 -0300

    fix: binds

--- lib/app/modules/timer/timer_module.dart
+++ lib/app/modules/timer/timer_module.dart
@@ -1,4 +1,5 @@
 import 'package:cuber_timer/app/core_module/core_module.dart';
+import 'package:cuber_timer/app/modules/home/home_module.dart';
 import 'package:cuber_timer/app/modules/timer/controller/count_down_controller.dart';
 import 'package:cuber_timer/app/modules/timer/controller/timer_controller.dart';
 import 'package:cuber_timer/app/modules/timer/presenter/timer_page.dart';
@@ -6,12 +7,11 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 class TimerModule extends Module {
   @override
-  List<Module> get imports => [CoreModule()];
+  List<Module> get imports => [CoreModule(), HomeModule()];
 
   @override
   void binds(Injector i) {
-    i.addSingleton<TimerController>(
-        () => TimerController(localDatabase: i(), recordController: i()));
+    i.addSingleton<TimerController>(TimerController.new);
     i.addSingleton<CountDownController>(CountDownController.new);
   }
 
