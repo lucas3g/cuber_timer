55b67f18dc6b3586a70a6f1775418afaec9001c4
Lucas Emanuel Silva <53888064+lucas3g@users.noreply.github.com>
2025-08-02 22:21:49 -0700

    feat: initialize iOS ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app/app_widget.dart';
 import 'app/di/dependency_injection.dart';
@@ -7,6 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await configureDependencies();
+  await MobileAds.instance.initialize();
 
   runApp(const AppWidget());
 }
